5264919f97e6a51f09af4c2aec9d8df1f570adc5
Te Vallee <astronomemusic@gmail.com>
2017-09-11 21:07:14 -1000

    added fade animations

---
+++ scripts/fade.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('.fade').hide(0).delay(100).fadeIn(500);
+  $('.columns').each(function(i, el) {
+    $(el).hide(0).delay(500*i).fadeIn(800);
+  });
+
+  $(window).scroll(function() {
+    $('.hidden').each(function(i, el) {
+      var bottomOfEl = $(this).position().top + $(this).outerHeight();
+      var bottomOfWindow = $(window).scrollTop() + $(window).height();
+      if( bottomOfWindow > bottomOfEl ){
+        $(this).animate({'opacity':'1'},500);
+      }
+    });
+  });
+});
