9bb83653b2d19bccf0f1b5caa0f146e16d0af9f8
Te Vallee <astronomemusic@gmail.com>
2017-09-23 16:11:32 -1000

    animation modifications

--- scripts/fade.js
+++ scripts/fade.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-  $('.fade').hide(0).delay(100).fadeIn(500);
-  $('.columns').each(function(i, el) {
-    $(el).hide(0).delay(500*i).fadeIn(800);
+  $(".fade").hide(0).delay(100).fadeIn(500);
+  $(".columns").each(function(i, el) {
+    $(el).hide(0).delay(500 * i).fadeIn(800);
   });
 
   $(window).scroll(function() {
-    $('.hidden').each(function(i, el) {
+    $(".hidden").each(function(i, el) {
       var bottomOfEl = $(this).position().top + $(this).outerHeight();
       var bottomOfWindow = $(window).scrollTop() + $(window).height();
-      if( bottomOfWindow > bottomOfEl ){
-        $(this).animate({'opacity':'1'},500);
+      if (bottomOfWindow > bottomOfEl) {
+        $(this).animate({ opacity: "1" }, 500);
       }
     });
   });
--- scripts/terminal.js
+++ scripts/terminal.js
@@ -1,54 +1,56 @@
 $(document).ready(function() {
-  var terminal = $('#terminal-content')[0];
-  var content = '<p>Hello World</p>';
+  var terminal = $("#terminal-content")[0];
+  var content = "<p>Hello World</p>";
   render(terminal, content, 0);
-  content = '<p>My name is Shante</p><p>I am a front end developer</p>';
+  content = "<p>My name is Shante</p><p>I am a front end developer</p>";
   render(terminal, content, 800);
-  content = '<br><p>Are you here to check out my portfolio?</p><p>> Enter [yes] or [no]  :  <span class="cursor"></span></p>';
+  content =
+    '<br><p>Are you here to check out my portfolio?</p><p>> Enter [yes] or [no]  :  <span class="cursor"></span></p>';
   render(terminal, content, 2000);
 
   setTimeout(function() {
-    var cursor = $('.cursor')[0];
-    content = 'y';
+    var cursor = $(".cursor")[0];
+    content = "y";
     render(cursor, content, 1000);
-    content = 'e';
+    content = "e";
     render(cursor, content, 1200);
-    content = 's';
+    content = "s";
     render(cursor, content, 1400);
 
     setTimeout(function() {
-      content = '<br><p>Initializing awesomeness...</p>';
+      content = "<br><p>Initializing awesomeness...</p>";
       render(terminal, content, 500);
-    
-      content = '<p>awesomeness complete!</p><br><p>Getting sick wubz...</p>';
+
+      content = "<p>awesomeness complete!</p><br><p>Getting sick wubz...</p>";
       render(terminal, content, 1000);
-      content = '<p>sick wubz complete!</p><br><p>Getting projects...</p>';
+      content = "<p>sick wubz complete!</p><br><p>Getting projects...</p>";
       render(terminal, content, 2000);
-      content = '<p>projects complete!</p><br><p>Getting resume...</p>';
+      content = "<p>projects complete!</p><br><p>Getting resume...</p>";
       render(terminal, content, 3000);
-      content = '<br><p>Getting resume...</p>';
-      render(terminal, content, 4000)
-      content = '<br><p>SUCCESS</p>';
+      content = "<br><p>Getting resume...</p>";
+      render(terminal, content, 4000);
+      content = "<br><p>SUCCESS</p>";
       render(terminal, content, 4000, true);
-    },1300);
-  },2000)
+    }, 1300);
+  }, 2000);
 });
 
 function render(targetElement, content, time, complete) {
   setTimeout(function() {
     targetElement.innerHTML += content;
-    var height = $('#terminal-content')[0].scrollHeight;
-    $('#terminal-content').scrollTop(height);
-    if(complete) {
-      $('.fade').hide(0).delay(100).fadeOut(500);
+    var height = $("#terminal-content")[0].scrollHeight;
+    $("#terminal-content").scrollTop(height);
+    if (complete) {
+      $(".fade").hide(0).delay(100).fadeOut(500);
       setTimeout(function() {
-        $('.terminal').addClass('hide');
-        $('.hidden').removeClass('hidden')
-        $('.fade').hide(0).delay(100).fadeIn(500);
-        $('.columns').each(function(i, el) {
-          $(el).hide(0).delay(500*i).fadeIn(800);
+        $(".terminal").addClass("hide");
+        $(".container").removeClass("hide");
+        $(".nav").removeClass("hide");
+        $(".fade").hide(0).delay(100).fadeIn(500);
+        $(".columns").each(function(i, el) {
+          $(el).hide(0).delay(500 * i).fadeIn(800);
         });
-      },500)
+      }, 500);
     }
-  },time)
+  }, time);
 }
